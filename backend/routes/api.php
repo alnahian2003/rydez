c9f31d085294475252d93f49e5d3fda1aec4a619
alnahian2003 <a.alnahian2003@gmail.com>
2023-04-16 21:07:13 +0600

    Sending Verification Code Successfully

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('login', [LoginController::class, 'login']);
+    Route::post('login/verify', [LoginController::class, 'verify']);
 });
